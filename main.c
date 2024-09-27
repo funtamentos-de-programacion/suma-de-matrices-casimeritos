0bfab2fc11f626f184bcd756e80b72454fd9d094
Emiliano <guerracordovaemiliano9@gmail.com>
2024-09-26 21:54:24 -0600

    Añadido la lectura de la seuçgunda matriz mas la impresion y suma correspondiente de ambas

--- main.c
+++ main.c
@@ -6,25 +6,44 @@ int main(){
   int m, n, p, q;
   int i, j;
 
-  do{
+  do {
     printf("Ingresa las dimensiones de la primer matriz\n");
     scanf("%d %d", &m, &n);
     printf("Ingresa las dimensiones de la segunda matriz\n");
     scanf("%d %d", &p, &q);
-  }while(m!=p || n!=q || m <=0 || n<=0 || p <= 0 ||  q <=0);
+  } while(m != p || n != q || m <= 0 || n <= 0 || p <= 0 || q <= 0);
 
-
-  for(i =0; i <m; i++){
-    for(j=0; j <n ; j++){
+  // Leer la primera matriz
+  printf("Ingresa los elementos de la primera matriz:\n");
+  for(i = 0; i < m; i++) {
+    for(j = 0; j < n; j++) {
       scanf("%d", &a[i][j]);
     }
   }
 
-  //TODO(1): Colocar el código para leer la segunda matriz
+  // TODO(1): Leer la segunda matriz
+  printf("Ingresa los elementos de la segunda matriz:\n");
+  for(i = 0; i < p; i++) {
+    for(j = 0; j < q; j++) {
+      scanf("%d", &b[i][j]);
+    }
+  }
 
-  //TODO(2): Colocar el código para sumar las matrices
+  // TODO(2): Sumar las matrices
+  for(i = 0; i < m; i++) {
+    for(j = 0; j < n; j++) {
+      c[i][j] = a[i][j] + b[i][j];
+    }
+  }
 
-  //TODO(3): Colocar el código para mostrar la matriz C
+  // TODO(3): Mostrar la matriz resultante
+  printf("La matriz resultante de la suma es:\n");
+  for(i = 0; i < m; i++) {
+    for(j = 0; j < n; j++) {
+      printf("%d ", c[i][j]);
+    }
+    printf("\n");
+  }
 
   return 0;
 }
